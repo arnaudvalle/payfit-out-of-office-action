2271e1a5cd6c4438d438dc8ead809a5582b2a85e
Arnaud Valle <arnaud.valle@gmail.com>
2024-06-06 21:58:16 +0200

    feat: filter out only VEVENTs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { async as icalAsync } from "node-ical";
+import { VEvent, async as icalAsync } from "node-ical";
 
 (async () => {
   try {
@@ -6,7 +6,23 @@ import { async as icalAsync } from "node-ical";
       throw new Error("CALENDAR_URL is missing");
     }
 
-    const events = await icalAsync.fromURL(process.env.CALENDAR_URL);
+    const response = await icalAsync.fromURL(process.env.CALENDAR_URL);
+
+    const events = Object.values(response).filter(
+      (component): component is VEvent => component.type === "VEVENT",
+    );
+
+    // Don't bother going further if there aren't any actual events
+    if (events.length === 0) {
+      console.info("No events found");
+      return;
+    }
+
+    const employees = ["Firstname Lastname"];
+
+    const employeesEvents = events.filter(
+      ({ summary }) => summary === `Absence - ${employees[0]}`,
+    );
   } catch (err: unknown) {
     console.log(err);
 
